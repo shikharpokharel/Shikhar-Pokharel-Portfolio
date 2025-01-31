bfe48d63f17e734856655ae8b000834235f8aef6
Shikhar Pokharel <pokharelshikharu@gmail.com>
2025-01-31 19:12:59 +0545

    Portfolio Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@
                     // Update the counter value after the page has fully loaded
                     document.addEventListener("DOMContentLoaded", function() {
                        document.getElementById("experienceCounter").innerText = totalYears;
-                       experienceCounter.setAttribute('data-purecounter-end', totalYears);
+                       experienceCounter.setAttribute('data-purecounter-end', totalYears + '+');
                     });
   
   function updateDateTime() {
@@ -293,3 +293,25 @@ updateDateTime();
   });
 
 })()
+
+// (function() {
+//   // Add event listener
+//   document.addEventListener("mousemove", parallax);
+//   const elem = document.querySelector(".parallax");
+  
+//   console.log('***',elem)
+//   // Magic happens here
+//   function parallax(e) {
+//       let _w = window.innerWidth/2;
+//       let _h = window.innerHeight/2;
+//       let _mouseX = e.clientX;
+//       let _mouseY = e.clientY;
+//       let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+//       let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+//       let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+//       let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+//       console.log(x);
+//       elem.style.backgroundPosition = x;
+//   }
+
+// })();
